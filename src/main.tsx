6ec4b67257ee3dacb05b00321158e6a09dd0d0db
cesaraugusto316 <cesaraugusto316@protonmail.com>
2023-10-09 20:00:32 -0500

    ✨ redirection

--- src/main.tsx
+++ src/main.tsx
@@ -9,10 +9,8 @@ import './index.css';
 ReactDOM
   .createRoot(document.getElementById('root') as HTMLElement)
   .render(
-    // <React.StrictMode>
     <BrowserRouter>
       <ScrollToTop />
       <App />
     </BrowserRouter>,
-    // </React.StrictMode>,
   );
